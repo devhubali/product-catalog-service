ec2e05b46a4805c0254fce14d1ec33e9063fd165
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-05-24 10:14:24 +0300

    get all brands implementation

--- catalog/src/main/java/com/catalog/brand/service/BrandServiceImpl.java
+++ catalog/src/main/java/com/catalog/brand/service/BrandServiceImpl.java
@@ -1,8 +1,21 @@
 package com.catalog.brand.service;
 
+import com.catalog.brand.dto.BrandResponse;
+import com.catalog.brand.repository.BrandRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BrandServiceImpl implements BrandService{
+    private final BrandRepository brandRepository;
 
+    public BrandServiceImpl(BrandRepository brandRepository){
+        this.brandRepository = brandRepository;
+    }
+
+    @Override
+    public List<BrandResponse> getAllBrands() {
+        return brandRepository.findAll().stream().map(BrandResponse::from).toList();
+    }
 }
