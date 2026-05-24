72076ad16b5ecebe7ebf456bcff3f6292af72ac0
vnxkaito <81317627+vnxkaito@users.noreply.github.com>
2026-05-24 15:25:17 +0300

    added variant entity

---
+++ catalog/src/main/java/com/catalog/variant/repository/VariantRepository.java
@@ -0,0 +1,11 @@
+package com.catalog.variant.repository;
+
+import com.catalog.variant.entity.Variant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VariantRepository extends JpaRepository<Variant, UUID> {
+}
